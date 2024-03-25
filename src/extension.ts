e4d1ab1669f261a7c6b4a952ea9b8c7aed0b30be
Szlachu <48260901+lukiszlak@users.noreply.github.com>
2024-03-25 20:23:25 +0100

    Changed name of the command

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('powershell-in-kotlin-dsl.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('powershell-in-kotlin-dsl.pwshInNewWindow', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Powershell In Kotlin DSL!');
+		vscode.window.showInformationMessage('Your powershell script is in new window');
 	});
 
 	context.subscriptions.push(disposable);
