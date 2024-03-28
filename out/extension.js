0ddd4be0ebecd11d211d9e61d7cfdab79f1ef110
Szlachu <48260901+lukiszlak@users.noreply.github.com>
2024-03-28 19:37:06 +0100

    Added parsing the output in new window to have a working powershell script

--- out/extension.js
+++ out/extension.js
@@ -40,6 +40,63 @@ function activate(context) {
         });
         vscode.window.showTextDocument(document);
     }
+    function escapeSpecialCharacters(text, escape = true) {
+        const escapeMap = {
+            '\\': '\\\\',
+            '^': '\\^',
+            '$': '\\$',
+            '*': '\\*',
+            '+': '\\+',
+            '?': '\\?',
+            '.': '\\.',
+            '(': '\\(',
+            ')': '\\)',
+            '[': '\\[',
+            ']': '\\]',
+            '{': '\\{',
+            '}': '\\}',
+            '|': '\\|',
+            '/': '\\/',
+        };
+        if (escape) {
+            return text.replace(/[\\^$*+?.()|[\]{}\/]/g, match => escapeMap[match] || match);
+        }
+        else {
+            const reverseEscapeMap = {};
+            for (const key in escapeMap) {
+                if (Object.prototype.hasOwnProperty.call(escapeMap, key)) {
+                    reverseEscapeMap[escapeMap[key]] = key;
+                }
+            }
+            return text.replace(/\\([\\^$*+?.()|[\]{}\/])/g, (match, escapedChar) => reverseEscapeMap[match] || match);
+        }
+    }
+    function parsePowershell(content, toPowershell) {
+        const kotlinToPowershell = {
+            "${'$'}": "$"
+        };
+        const powershellToKotlin = {
+            "$": "${'$'}"
+        };
+        var mapping;
+        if (toPowershell) {
+            mapping = kotlinToPowershell;
+        }
+        else {
+            mapping = powershellToKotlin;
+        }
+        let pattern = '';
+        const keys = Object.keys(mapping);
+        for (let i = 0; i < keys.length; i++) {
+            const key = escapeSpecialCharacters(keys[i]);
+            pattern += key + '|';
+        }
+        // Remove the last '|'
+        pattern = pattern.slice(0, -1);
+        const patternRegExp = new RegExp(pattern, 'g');
+        var output = content.replace(patternRegExp, match => mapping[match] || "*error*");
+        return output;
+    }
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
@@ -48,13 +105,14 @@ function activate(context) {
         // Display a message box to the user
         const editor = vscode.window.activeTextEditor;
         const selection = editor.selection;
-        var highlighted = "Nothing";
+        var highlightedText = "Nothing";
         if (selection && !selection.isEmpty) {
             const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-            highlighted = editor.document.getText(selectionRange);
+            highlightedText = editor.document.getText(selectionRange);
         }
-        openInUntitled(highlighted, "powershell");
-        vscode.window.showInformationMessage(`You selected:\n${highlighted}`);
+        var parsedScript = parsePowershell(highlightedText, true);
+        openInUntitled(parsedScript, "powershell");
+        vscode.window.showInformationMessage(`You selected:\n${parsedScript}`);
     });
     context.subscriptions.push(disposable);
 }
