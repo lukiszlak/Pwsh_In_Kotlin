65ba63217b9d127d8a32989d382e4b0e0f166d72
Szlachu <48260901+lukiszlak@users.noreply.github.com>
2024-03-29 18:00:47 +0100

    Added generating back selection range

--- out/extension.js
+++ out/extension.js
@@ -91,7 +91,7 @@ function activate(context) {
         return output;
     }
     function getReturnInfo(content) {
-        var match = content.match(/(<@(.*?)@> <#(.*?)#>)/);
+        var match = content.match(/(<@(.*?)@> <#sl(.*?);sc(.*?);el(.*?);ec(.*?)#>)/);
         return match;
     }
     // The command has been defined in the package.json file
@@ -101,17 +101,17 @@ function activate(context) {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         const editor = vscode.window.activeTextEditor;
-        const selection = editor.selection;
-        const currentFileName = editor.document.fileName;
+        const selection = editor?.selection;
+        const currentFileName = editor?.document.fileName;
         var highlightedText = "Nothing";
-        var fileLanguage = editor.document.languageId;
+        var fileLanguage = editor?.document.languageId;
         var selectionRange;
         if (selection && !selection.isEmpty) {
             selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-            highlightedText = editor.document.getText(selectionRange);
+            highlightedText = editor?.document.getText(selectionRange);
         }
         if (fileLanguage == "kotlin") {
-            highlightedText += `\n\n\n### DO NOT DELETE <@${currentFileName}@> <#${selectionRange}#> DO NOT DELETE ###`;
+            highlightedText += `\n\n\n### DO NOT DELETE <@${currentFileName}@> <#sl${selectionRange?.c.c};sc${selectionRange?.c.e};el${selectionRange?.e.c};ec${selectionRange?.e.e}#> DO NOT DELETE ###`;
         }
         var parsedScript = "";
         if (fileLanguage == "kotlin") {
@@ -123,8 +123,9 @@ function activate(context) {
             var parsedScript = parseScript(highlightedText, false);
             var returnInfo = getReturnInfo(parsedScript);
             var filePath = vscode.Uri.parse(`file:///${returnInfo[2]}`);
-            var originalSelection = returnInfo[3];
+            var originalSelection = new vscode.Range(parseInt(returnInfo[3]), parseInt(returnInfo[4]), parseInt(returnInfo[5]), parseInt(returnInfo[6]));
             openOldWindow(filePath);
+            vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(parsedScript), originalSelection);
             vscode.window.showInformationMessage("Opened in Powershell");
         }
     });
--- src/extension.ts
+++ src/extension.ts
@@ -76,8 +76,7 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	function getReturnInfo(content: String) {
-
-		var match = content.match(/(<@(.*?)@> <#(.*?)#>)/);
+		var match = content.match(/(<@(.*?)@> <#sl(.*?);sc(.*?);el(.*?);ec(.*?)#>)/);
 		return match;
 	}
 
@@ -88,19 +87,19 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		const editor = vscode.window.activeTextEditor;
-		const selection = editor.selection;
-		const currentFileName = editor.document.fileName;
+		const selection = editor?.selection;
+		const currentFileName = editor?.document.fileName;
 		var highlightedText = "Nothing";
-		var fileLanguage = editor.document.languageId;
+		var fileLanguage = editor?.document.languageId;
 		var selectionRange;
 
 		if (selection && !selection.isEmpty) {
 			selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-			highlightedText = editor.document.getText(selectionRange);
+			highlightedText = editor?.document.getText(selectionRange);
 		}
 		
 		if (fileLanguage == "kotlin") {
-			highlightedText += `\n\n\n### DO NOT DELETE <@${currentFileName}@> <#${selectionRange}#> DO NOT DELETE ###`;
+			highlightedText += `\n\n\n### DO NOT DELETE <@${currentFileName}@> <#sl${selectionRange?.c.c};sc${selectionRange?.c.e};el${selectionRange?.e.c};ec${selectionRange?.e.e}#> DO NOT DELETE ###`;
 		}
 
 		var parsedScript = "";
@@ -112,9 +111,10 @@ export function activate(context: vscode.ExtensionContext) {
 			var parsedScript = parseScript(highlightedText, false);
 			var returnInfo = getReturnInfo(parsedScript);
 			var filePath = vscode.Uri.parse(`file:///${returnInfo[2]}`);
-			var originalSelection = returnInfo[3];
+			var originalSelection = new vscode.Range(parseInt(returnInfo[3]), parseInt(returnInfo[4]), parseInt(returnInfo[5]), parseInt(returnInfo[6]));
 
 			openOldWindow(filePath);
+			vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(parsedScript), originalSelection);
 			vscode.window.showInformationMessage("Opened in Powershell");
 		}
 	});
